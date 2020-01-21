db5088aabd7e06c8b5618ef855451c9040f81205
sidan123 <sidan@baabte.com>
2020-01-21 14:36:52 +0530

    file upload corrected

--- fadapp/urls.py
+++ fadapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns =[
     path('', views.user_detail),
@@ -15,5 +13,3 @@ urlpatterns =[
     path('editprofile/', views.fn_edit_profile),
     path('viewfabrics',views.fn_viewfabrics)
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
